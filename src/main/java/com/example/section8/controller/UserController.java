e4a375de708ab55d2955cf17757127e072beb065
duy2003cp <duylpph20732@fpt.edu.vn>
2023-09-11 00:44:05 +0700

    add global exception handler

--- src/main/java/com/example/section8/controller/UserController.java
+++ src/main/java/com/example/section8/controller/UserController.java
@@ -85,4 +85,16 @@ public class UserController {
         );
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails>handleGlobalException(Exception exception,
+                                                                        WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                webRequest.getDescription(false),
+                "INTERNAL SERVER ERROR"
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
 }
